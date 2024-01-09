661f64268dac890c936adafc2d75d11b302b3486
sunil kurthakoti <sunilkurthakoti06@gmail.com>
2024-01-09 19:16:14 +0530

    initial commit

---
+++ application-code/app-tier/DbConfig.js
@@ -0,0 +1,6 @@
+module.exports = Object.freeze({
+    DB_HOST : 'database-1-instance-1.c5u47n9pp5by.us-east-1.rds.amazonaws.com',
+    DB_USER : 'admin',
+    DB_PWD : 'admin1234',
+    DB_DATABASE : 'webappdb'
+});
